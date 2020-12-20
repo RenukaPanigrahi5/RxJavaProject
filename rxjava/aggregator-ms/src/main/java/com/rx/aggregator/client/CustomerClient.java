38e7af29e6cb065f0989b7daa70364c13f1e1492
Renuka Panigrahi <panigrahirenuka599@gmail.com>
2020-12-20 18:42:39 -0500

    changes

--- rxjava/aggregator-ms/src/main/java/com/rx/aggregator/client/CustomerClient.java
+++ rxjava/aggregator-ms/src/main/java/com/rx/aggregator/client/CustomerClient.java
@@ -9,7 +9,7 @@ public class CustomerClient {
     public Single<CustomerDto> getCustomer(int id){
         return Single.create(
                 emitter -> {
-                    if(id < 50){
+                    if(id > 50){
                         emitter.onError(new RuntimeException("Not a valid Customer"));
                     }else {
                         emitter.onSuccess(new CustomerDto(id,"First-" +id,"Last-" +id));
