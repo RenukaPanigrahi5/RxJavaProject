330d715dc325e97903c641b31ab3edaafbdc36a0
Renuka Panigrahi <panigrahirenuka599@gmail.com>
2020-12-20 18:37:06 -0500

    customer init

---
+++ rxjava/customer-ms/src/main/java/com/java/rx/customer/controller/CustomerController.java
@@ -0,0 +1,9 @@
+package com.java.rx.customer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+}
