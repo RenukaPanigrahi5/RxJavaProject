37b23bfbc1b91860d30c4189b91704c0f8216e28
Renuka Panigrahi <panigrahirenuka599@gmail.com>
2020-12-24 12:30:53 -0500

    Aggregator changes

--- rxjava/aggregator-ms/src/main/java/com/rx/aggregator/controller/AggregatorController.java
+++ rxjava/aggregator-ms/src/main/java/com/rx/aggregator/controller/AggregatorController.java
@@ -5,6 +5,7 @@ import com.rx.aggregator.client.CustomerClient;
 import com.rx.aggregator.model.AccountDto;
 import com.rx.aggregator.model.CustomerDto;
 import com.rx.aggregator.model.TransactionDto;
+import io.reactivex.Observable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,9 +24,58 @@ public class AggregatorController {
     private AccountClient accountClient;
 
     @GetMapping("/{customerId}")
-    public CustomerDto getAllCustomerInfo(@PathVariable("customerId") String customerId) {
+    public CustomerDto getAllCustomerInfo(@PathVariable("customerId") final String customerId) {
         //use CustomerClient and AccountClient actually to get data.
-        return sampleData();
+        // Attempt1
+        /*return this.customerClient.
+                getCustomer(Integer.parseInt(customerId))
+                .flatMap(customer -> {
+                    return accountClient.getAccounts(customer.getId()).toList()
+                            .map(accounts -> {
+                                CustomerDto cust = new CustomerDto();
+                                cust.setAccounts(accounts);
+                                return cust;
+                            });
+                }).flatMap(
+                        customerDto -> {
+                            return Observable.fromIterable(customerDto.getAccounts())
+                                    .map(
+                                            account -> accountClient.getTransactions(customerDto.getId(), account.getId())
+                                    ).toList()
+                                    .map(transactions -> {
+                                        // for each account set the transaction
+                                        //each customer set the account + transaction
+                                        AccountDto account1 = new AccountDto();
+                                        account1.setTransactions(account1.getTransactions());
+                                        CustomerDto customers =  new CustomerDto();
+                                        customers.setAccounts(transactions);
+                                        return customers;
+                                    });
+                        }
+                ).blockingGet();*/
+        return this.customerClient
+                .getCustomer(Integer.parseInt(customerId))
+                .flatMap(customer -> {
+                            return accountClient.getAccounts(customer.getId())
+                                    .flatMap(
+                                            account -> {
+                                                return this.accountClient.getTransactions(Integer.parseInt(customerId), account.getId())
+                                                        .toList().map(
+                                                                txnList -> {
+                                                                    account.setTransactions(txnList);
+                                                                    return account;
+                                                                }
+                                                        ).toObservable();
+                                            }
+                                    ).toList()
+                                    .map(accountList -> {
+                                        customer.setAccounts(accountList);
+                                        return customer;
+                                    });
+                        }
+
+                ).blockingGet();
+
     }
 
     public CustomerDto sampleData() {
